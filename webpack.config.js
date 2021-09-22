f1b50ef32278d1c776ab9d6a24e7e6a458fdd174
akhil nukala <32026429+akhilnukala@users.noreply.github.com>
2021-09-22 17:04:45 -0500

    set mode to development

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 // must export an object - with a couple of properties
 module.exports = {
+  mode: "development", // default for mode is production
+  // NOTE: webpack does not minify code in development mode
   entry: "./src/index.js", // default for entry is index.js
   output: {
     // default for output is dist/main.js
