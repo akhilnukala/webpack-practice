993ee07ff6b1f9a1b336e7d83a845237216caad5
akhil nukala <32026429+akhilnukala@users.noreply.github.com>
2021-09-24 02:31:17 -0500

    cache busting using content-hash & html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // must export an object - with a couple of properties
 module.exports = {
-  mode: "development", // default for mode is production
-  // NOTE: webpack does not minify code in development mode
-  entry: "./src/index.js", // default for entry is index.js
+  // default for mode is production
+  mode: "development", // NOTE: webpack does not minify code in development mode
+  // default for entry is index.js
+  entry: "./src/index.js",
+  // default for output is dist/main.js
   output: {
-    // default for output is dist/main.js
-    filename: "main.js",
+    /*
+    ABOUT CONTENT-HASH
+    browsers usually cache/save files based on their names
+    PROBLEM:
+    if we made some changes to our main.js bundle file and give it to the browser, the browser might ignore the updated main.js and use the stale main.js from its cache - since the names match
+    SOLUTION:
+    to prevent this content-hash is used
+    using content-hash webpack creates the name of output bundle file based on the its content, so if the content changes, name of the outut bundle file also changes, thus preventing the browser from using the stale ouput bundle file
+    */
+    filename: "main-[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
-
   /*
   ABOUT LOADERS
   webpack can bundle different types of files other than javascript
@@ -50,4 +60,19 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    /*
+    ABOUT HTML-WEBPACK-PLUGIN
+    WHY USE?
+    since we cannot add a content-hashed main.js bundle file to our static HTML file, we need to use html-webpack-plugin to create a HTML file and inject the content-hashed main.js bundle file in that HTML file
+    USAGE:
+    1. without passing any argument i.e., new HtmlWebpackPlugin() -
+        this plugin create a small HTML file in the output folder and will place a <script> tag at the end with "src" attribute pointing to the content-hashed main.js bundle file
+    1. by passing a custom HTML file as the template -
+        this plugin will take the passed HTML file as the template and create a new HTML file in the output folder and will place a <script> tag at the end with "src" attribute pointing to the content-hashed main.js bundle file
+    */
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
 };
