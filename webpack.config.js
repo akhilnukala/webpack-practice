3707a81b703a582a227e1252efd63639f1d8036d
akhil nukala <32026429+akhilnukala@users.noreply.github.com>
2021-09-24 01:11:35 -0500

    Add css-loader & style-loader to handle css files

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,33 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+
+  /*
+  ABOUT LOADERS
+  webpack can bundle different types of files other than javascript
+  webpack uses loaders which dictate how files other than javascript should be pre-processed when they are imported or loaded into a javascript file
+  */
+  module: {
+    // in module property, we specify different rules for different types of files
+    rules: [
+      {
+        // we apply css-loader & style-loader to all css files
+        /*
+        ABOUT CSS LOADER
+        css-loader takes css
+        AND
+        turns it into valid javascript code (i.e., into a string)
+        AND
+        adds that code to the bundled .js file
+        */
+        /*
+        ABOUT STYLE LOADER
+        style-loader takes the css code that is converted to javascript from the bundled .js file and injects it into DOM using the <style> tag
+        */
+        test: /\.css$/,
+        // NOTE: order is important here
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
